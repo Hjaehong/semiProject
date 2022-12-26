30fd4b9d17cf28177f27978728a359accb86ff22
Hjaehong <zero5140@naver.com>
2022-12-26 17:41:39 +0900

    first commit

---
+++ src/main/java/com/team/semitravelrecomand/SemiTravelRecomandApplication.java
@@ -0,0 +1,13 @@
+package com.team.semitravelrecomand;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SemiTravelRecomandApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SemiTravelRecomandApplication.class, args);
+	}
+
+}

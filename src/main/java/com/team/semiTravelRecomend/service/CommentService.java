b692c538d1f2419659647e414158a7d26fe05879
Hjaehong <zero5140@naver.com>
2022-12-27 22:52:09 +0900

    댓글기능 중간 저장 1227

---
+++ src/main/java/com/team/semiTravelRecomend/service/CommentService.java
@@ -0,0 +1,7 @@
+package com.team.semiTravelRecomend.service;
+
+import com.team.semiTravelRecomend.model.dto.CommentDTO;
+
+public interface CommentService {
+    boolean registComment(CommentDTO comment);
+}

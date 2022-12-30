3f21d5234f9e96aaabe089252beaa26a35a814ea
Jihee <wlgml6925@gmail.com>
2022-12-30 20:33:37 +0900

    config 폴더 생성 및 기본 설정 완료

--- src/main/java/com/team/semiTravelRecommend/controller/record/RecordController.java
+++ src/main/java/com/team/semiTravelRecommend/controller/record/RecordController.java
@@ -8,15 +8,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
     * Version : 1.0
    * 클래스명: RecordController
    * 작성일자 : 2022/12/27
- * 작성자 : heojaehong
+   * 작성자 : heojaehong
    * 설명 : 여행기록 컨트롤러
    * 수정일자 :
    * 수정자 :
    * 수정내역 :
  */
 @Controller
-
-
 @RequestMapping("/record")
 public class RecordController {
 

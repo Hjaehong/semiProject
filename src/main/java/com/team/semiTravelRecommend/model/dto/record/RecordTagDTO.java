37b9e692d867740da4a8ec659053d809d8a4138b
Jihee <wlgml6925@gmail.com>
2023-01-03 00:53:02 +0900

    여행기록-태그 DTO, 태그DTO 추가

---
+++ src/main/java/com/team/semiTravelRecommend/model/dto/record/RecordTagDTO.java
@@ -0,0 +1,10 @@
+package com.team.semiTravelRecommend.model.dto.record;
+
+import lombok.Data;
+
+@Data
+public class RecordTagDTO {
+
+    private int recordNo;
+    private String tagCode;
+}
---
+++ src/main/java/com/team/semiTravelRecommend/model/dto/record/TagDTO.java
@@ -0,0 +1,10 @@
+package com.team.semiTravelRecommend.model.dto.record;
+
+import lombok.Data;
+
+@Data
+public class TagDTO {
+
+    private String tagCode;
+    private String tagName;
+}

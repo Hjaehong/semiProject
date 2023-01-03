602210fea4d8e3aa986fcc9daf801f3b99a7e49f
Hjaehong <zero5140@naver.com>
2023-01-03 10:50:05 +0900

    취향 선택시 취향에 맞는 장소 리스트 출력

--- src/main/java/com/team/semiTravelRecomend/controller/RecommendController.java
+++ src/main/java/com/team/semiTravelRecomend/controller/RecommendController.java
@@ -1,7 +1,65 @@
 package com.team.semiTravelRecomend.controller;
 
+import com.team.semiTravelRecomend.model.dto.PlaceDTO;
+import com.team.semiTravelRecomend.model.dto.TagDTO;
+import com.team.semiTravelRecomend.service.RecommendService;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.List;
+
+/**
+    * Version : 1.0
+   * 클래스명: RecommendController
+   * 작성일자 : 2023/01/02
+ * 작성자 : heojaehong
+   * 설명 : 컨트롤러
+   * 수정일자 :
+   * 수정자 :
+   * 수정내역 :
+ */
 @Controller
+@RequestMapping("/recommend")
 public class RecommendController {
+
+    private final RecommendService recommendService;
+
+    // 생성자 주입
+    public RecommendController(RecommendService recommendService) {
+        this.recommendService = recommendService;
+    }
+
+
+    // 추천 여행장소 리스트 보여주는 메소드
+    @GetMapping("/travelRecommend")
+    public Model recommendList(Model model){
+        List<PlaceDTO> travelList = recommendService.showRecommend();
+        List<TagDTO> tagList = recommendService.showTag();
+        model.addAttribute("travelList", travelList);
+        model.addAttribute("tagList", tagList);
+
+        return model;
+    }
+    // 취향에 맞춰 여행지를 보여주는 메소드
+    @PostMapping("/travelRecommend")
+    public void checkTag(@RequestParam(value = "tag", required = false) String tag_code,Model model){
+
+        List<PlaceDTO> travelList = recommendService.tagRecommendTravel(tag_code);
+        List<TagDTO> tagList = recommendService.showTag();
+
+        model.addAttribute("travelList", travelList);
+        model.addAttribute("tagList", tagList);
+        System.out.println("tag_code = " + tag_code);
+    }
+
+    // 여행지 클릭시 디테일 보여주는 메소드
+    @GetMapping("/placeDetail")
+    public void detail(){
+
+    }
 }
---
+++ src/main/java/com/team/semiTravelRecomend/model/dto/TagDTO.java
@@ -0,0 +1,19 @@
+package com.team.semiTravelRecomend.model.dto;
+
+import lombok.Data;
+ /**
+    * Version : 1.0
+   * 클래스명: TagDTO
+   * 작성일자 : 2023/01/02
+ * 작성자 : heojaehong
+   * 설명 : 태그 DTO
+   * 수정일자 :
+   * 수정자 :
+   * 수정내역 :
+ */
+@Data
+public class TagDTO {
+
+    private String tag_code;
+    private String tag_name;
+}

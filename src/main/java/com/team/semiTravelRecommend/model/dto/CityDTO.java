c5c8620a18543870a35439f1803b363d49e60fef
Hjaehong <zero5140@naver.com>
2023-01-16 15:10:31 +0900

    댓글 수정 삭제 기능 구현

--- src/main/java/com/team/semiTravelRecommend/model/dto/CityDTO.java
+++ src/main/java/com/team/semiTravelRecommend/model/dto/CityDTO.java
@@ -2,23 +2,11 @@ package com.team.semiTravelRecommend.model.dto;
 
 import lombok.Data;
 
-/**
-    * Version : 1.0
-   * 클래스명: CityDTO
-   * 작성일자 : 2023/01/01
- * 작성자 : heojaehong
-   * 설명 : 도시DTO
-   * 수정일자 :
-   * 수정자 :
-   * 수정내역 :
- */
-
- @Data
+@Data
 public class CityDTO {
 
-     private String cityCode;
-     private String locCode;
-     private String cityName;
-     private String badgeImg;
-     LocationDTO locationDTO;
+    private String cityCode;
+    private String cityName;
+    private String badgeImg;
+    private LocationDTO locationDTO;
 }
